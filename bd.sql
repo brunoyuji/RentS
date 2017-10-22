a098f1b1ad42062851d5c9e816151c7b34426089
brunoyuji <bruno.ishiyama@usp.br>
2017-10-22 20:37:43 -0200

    latest version 2017_10_22

--- bd.sql
+++ bd.sql
@@ -42,37 +42,33 @@ create table address ( id_address integer primary key AUTO_INCREMENT,
 create table products (
 			id_product INTEGER  primary keY AUTO_INCREMENT,
             id_user int,
-            id_category int,
 			product_name varchar(255) not null,
             product_status boolean not null,
             image varchar(200),
 			title varchar(300) not null,
-			price numeric(15,2) not null,
+			price decimal(15,2) not null,
             money_type varchar(15),
             period varchar(255) not null,
 			available_amount int not null,
             operation_amount int not null,
+			tags varchar(255) not null,
 			description text not null,
 			rating double
 );
 
-create table tags_and_ads (
+create table products_categories (
 			id_product int,
-            id_tag int
+			id_category int
 );
 
 
 create table categories (
 			id_category integer primary key AUTO_INCREMENT,
 			category_name varchar(255) not null,
-			category_description text not null
+			category_description text not null,
+			primary_category boolean
 );
 
-create table tags (
-			id_tag integer primary key AUTO_INCREMENT,
-			tag_name varchar(255) not null,
-			tag_description text not null
-);
 
 create table ad_complaint(
 			id_complaint integer primary key AUTO_INCREMENT,
@@ -87,8 +83,8 @@ create table ad_complaint(
 
 create table request(
 			id_request integer primary key AUTO_INCREMENT ,
-			id_renter int,
-			id_product int,
+			id_renter int not null,
+			id_product int not null,
             resquest_status varchar(255) not null,
             start_date datetime not null,
 			devolution_date datetime not null
@@ -96,7 +92,15 @@ create table request(
 
 create table rental(
 			id_request int,
-            process_status varchar(255) not null
+            process_status varchar(255) not null,
+            payed_to_the_store double not null
+);
+
+create table payment(
+			id_request int,
+			price decimal,
+			type int,
+			confirmation_date date
 );
 
 create table cancelled(
@@ -108,45 +112,41 @@ create table cancelled(
 create table landlord_rating(
 		id_request int,
 		rating int,
-        title_rating varchar(255),
-        description_rating varchar(255)
+        review varchar(255)
 );
 
 
 create table renter_rating(
 		id_request int,
 		rating int,
-        title_rating varchar(255),
-        description_rating varchar(255)
+        review varchar(255)
 );
 
 
 create table product_rating(
 		id_request int,
 		rating int,
-        title_rating varchar(255),
-        description_rating varchar(255)
+        review varchar(255)
 );
 
 ALTER TABLE landlord ADD FOREIGN KEY(id_user) REFERENCES user_profile(id_user);
 ALTER TABLE renter ADD FOREIGN KEY(id_user) REFERENCES user_profile(id_user);
 ALTER TABLE supervisor ADD FOREIGN KEY(id_user) REFERENCES user_profile(id_user);
 ALTER TABLE address ADD FOREIGN KEY(id_user) REFERENCES user_profile(id_user);
-ALTER TABLE products ADD FOREIGN KEY(id_user) REFERENCES user_profile(id_user);
-ALTER TABLE products ADD FOREIGN KEY(id_category) REFERENCES categories(id_category);
+ALTER TABLE products ADD FOREIGN KEY(id_user) REFERENCES landlord(id_user);
 
-ALTER TABLE tags_and_ads ADD FOREIGN KEY(id_product) REFERENCES products(id_product);
-ALTER TABLE tags_and_ads ADD FOREIGN KEY(id_tag) REFERENCES tags(id_tag);
 
 ALTER TABLE ad_complaint ADD FOREIGN KEY(id_product) REFERENCES products(id_product);
 ALTER TABLE ad_complaint ADD FOREIGN KEY(id_renter) REFERENCES user_profile(id_user);
 ALTER TABLE ad_complaint ADD FOREIGN KEY(id_supervisor) REFERENCES user_profile(id_user);
 
-ALTER TABLE request ADD FOREIGN KEY(id_renter) REFERENCES user_profile(id_user);
 ALTER TABLE request ADD FOREIGN KEY(id_product) REFERENCES products(id_product);
-
+ALTER TABLE request ADD FOREIGN KEY(id_renter) REFERENCES renter(id_user);
 
 ALTER TABLE rental ADD FOREIGN KEY(id_request) REFERENCES request(id_request);
+
+ALTER TABLE payment ADD FOREIGN KEY(id_request) REFERENCES rental(id_request);
+
 
 ALTER TABLE cancelled ADD FOREIGN KEY(id_request) REFERENCES request(id_request);
 
